38fc9f169bcd5c489c15b13b22924ef9698774fd
AyanAnsarii91 <ayancoder786@gmail.com>
2025-04-07 18:20:38 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Text-Converter/",
+  base: "/",
   plugins: [react()],
 });
